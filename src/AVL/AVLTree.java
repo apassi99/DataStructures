f5557d08762dfd514e5a0a2e02848183f8beb652
Arjun Passi <apassi@live.com>
2013-12-25 01:09:12 -0500

    AVL Tree Implemented

---
+++ src/AVL/AVLTree.java
@@ -0,0 +1,359 @@
+package AVL;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * AVL tree is an implementation of a self-balancing
+ * binary search tree. The data elements inserted in
+ * the tree must be comparable.
+ * 
+ * @author Arjun Passi
+ *
+ */
+public class AVLTree <T extends Comparable<? super T>>{
+	
+	/**
+	 * This class represents a AVLTreeNode that stores
+	 * the data element, both its children, and the height.
+	 * @author Arjun Passi
+	 *
+	 */
+	private class AVLTreeNode{
+		
+		/** Reference to the data element stored in the node. */
+		private T mData;
+		
+		/** Reference to the left child of the node */
+		private AVLTreeNode mLeft;
+		
+		/** Reference to the right child of the node*/
+		private AVLTreeNode mRight;
+		
+		/** Reference to the height of the node*/
+		private int mHeight;
+		
+		/**
+		 * Constructs a new AVL tree node.
+		 * @param element
+		 * @param leftChild
+		 * @param rightChild
+		 */
+		public AVLTreeNode(T data, AVLTreeNode left, AVLTreeNode right){
+			mData = data;
+			mLeft = left;
+			mRight = right;
+			mHeight = 0;
+		}
+	}
+	
+	/** Reference to the root of the tree*/
+	private AVLTreeNode mRoot;
+	
+	/** Reference to the size of the tree*/
+	private int mSize;
+	
+	/**
+	 * Constructs a new AVL tree
+	 */
+	public AVLTree(){
+		mRoot = null;
+		mSize = 0;
+	}
+	
+	/**
+	 * The "find" method allows to search a desired
+	 * data element in the tree.
+	 * @param data
+	 * @return the reference to the specified data element
+	 * otherwise null.
+	 */
+	public T find(T data){
+		if(data == null)
+			return null;
+		return find(mRoot, data);
+	}
+	
+	/**
+	 * This method recursively finds the provided
+	 * data element in the tree.
+	 * @param n
+	 * @param data
+	 * @return
+	 */
+	private T find(AVLTreeNode n, T data){
+		//Provided node is null
+		if(n == null)
+			return null;
+		if(n.mData.compareTo(data) == 0)
+			return n.mData;
+		else if(data.compareTo(n.mData) > 0)
+			return find(n.mRight, data);
+		else
+			return find(n.mLeft, data);
+	}
+
+	/**
+	 * This method allows to insert a data element
+	 * in the AVL tree.
+	 * @param data
+	 * @return true if the insertion was sucesfull else
+	 * false
+	 */
+	public boolean insert(T data){
+		if(data == null)
+			return false;
+		try{
+			mRoot = insert(mRoot, data);
+			mSize++;
+			return true;
+		} catch(IllegalArgumentException e){
+			return false;
+		}
+	}
+	
+	/**
+	 * Helper method to recursively insert a data element
+	 * in the tree. Throws an illegal argument exception
+	 * when duplicate is being added.
+	 * @param n
+	 * @param data
+	 * @return AVLTreeNode
+	 */
+	private AVLTreeNode insert(AVLTreeNode n, T data){
+		if(n == null)
+			return new AVLTreeNode(data, null, null);
+		
+		if(n.mData.compareTo(data) == 0)
+			throw new IllegalArgumentException("Duplicates not Allowed!");
+		else if(data.compareTo(n.mData) > 0)
+			n.mRight = insert(n.mRight, data);
+		else
+			n.mLeft = insert(n.mLeft, data);
+		
+		//Max Allowed height difference in an AVL tree is 1
+		if(Math.abs(difference(n)) != 1)
+			n = performRotation(n);
+		
+		//update the height of the node
+		n.mHeight = Math.max(height(n.mLeft), height(n.mRight)) + 1;
+		return n;
+	}
+	
+	/**
+	 * Method removes only one occurrence o
+	 * data element.
+	 * @param data
+	 * @return
+	 */
+	public boolean remove(T data){
+		if(data == null)
+			return false;
+		try{
+			mRoot = remove(mRoot, data);
+			mSize--;
+			return true;
+		} catch(IllegalArgumentException e){
+			return false;
+		}
+	}
+	
+	/**
+	 * Helper function to recursively find and remove
+	 * the element in the AVL tree. Throws an illegal
+	 * argument exception if the element is not in the
+	 * AVL tree.
+	 * @param n
+	 * @param data
+	 * @return AVLTreeNode
+	 */
+	private AVLTreeNode remove(AVLTreeNode n, T data){
+		if(n == null)
+			throw new IllegalArgumentException("Element Not Found!");
+		
+		if(n.mData.compareTo(data) == 0)
+			return remove(n);
+		else if(data.compareTo(n.mData) > 0)
+			n.mRight = remove(n.mRight, data);
+		else
+			n.mLeft = remove(n.mLeft, data);
+		
+		//Max Allowed height difference in an AVL tree is 1
+		if(Math.abs(difference(n)) != 1)
+			n = performRotation(n);
+				
+		//update the height of the node
+		n.mHeight = Math.max(height(n.mLeft), height(n.mRight)) + 1;
+		return n;
+	}
+	
+	/**
+	 * Helper method to remove the desired AVLTreeNode
+	 * from the AVL tree.
+	 * @param n
+	 * @return
+	 */
+	private AVLTreeNode remove(AVLTreeNode n){
+		if(n == null)
+			return null;
+		
+		if(n.mLeft == null && n.mRight == null)
+			return null;
+		else if(n.mLeft == null && n.mRight != null)
+			return n.mRight;
+		else if(n.mLeft != null && n.mRight == null)
+			return n.mLeft;
+		else {
+			T data = findMin(n.mRight);
+			n = remove(n, data);
+			n.mData = data;
+			return n;
+		}
+	}
+	
+	/**
+	 * Helper method to find the minimum data element
+	 * provided an AVL node.
+	 * @param subRoot
+	 * @return minimum element in the provided sub-tree
+	 */
+	private T findMin(AVLTreeNode subRoot){
+		if(subRoot == null)
+			return null;
+		if(subRoot.mLeft == null)
+			return subRoot.mData;
+		return findMin(subRoot.mLeft);
+	}
+	
+	/**
+	 * Method to retrieve the size of the tree.
+	 * @return size
+	 */
+	public int size(){
+		return mSize;
+	}
+	
+	/**
+	 * Removes all the elements in the AVL tree.
+	 */
+	public void removeAll(){
+		mSize = 0;
+		mRoot = null;
+	}
+	/**
+	 * Method to insert all the elements in the tree to
+	 * a list ordered in inOrderTraversal.
+	 * @return list of elements in InOrder
+	 */
+	public List<T> inOrderTraversal(){
+		ArrayList<T> list = new ArrayList<T>();
+		inOrderTraversal(mRoot, list);
+		return list;
+	}
+	
+	/**
+	 * Helper method to traverse the tree in InOrder.
+	 * @param n
+	 * @param list
+	 */
+	private void inOrderTraversal(AVLTreeNode n, ArrayList<T> list){
+		if(n == null)
+			return;
+		
+		inOrderTraversal(n.mLeft, list);
+		list.add(n.mData);
+		inOrderTraversal(n.mRight, list);
+	}
+	
+	/**
+	 * Helper method to perform a rotation on the provided
+	 * node based on the height difference of the left and
+	 * the right children.
+	 * @param n
+	 * @return new sub root
+	 */
+	private AVLTreeNode performRotation(AVLTreeNode n){
+		if(n == null) return null;
+		
+		//Rotation not required
+		if(Math.abs(difference(n)) != 2)
+			return n;
+		
+		if(difference(n) == 2 && difference(n.mLeft) == 1)
+			n = rightRotateLeftChild(n);
+		else if(difference(n) == 2 && difference(n.mLeft) == -1){
+			n.mLeft = leftRotateRightChild(n.mLeft);
+			n = rightRotateLeftChild(n);
+		}
+		else if(difference(n) == -2 && difference(n.mRight) == -1)
+			n = leftRotateRightChild(n);
+		else{
+			n.mRight = rightRotateLeftChild(n.mRight);
+			n = leftRotateRightChild(n);
+		}
+		return n;
+	}
+	
+	/**
+	 * This method performs a right rotation on the left
+	 * child of the provided node.
+	 * @param n
+	 * @return the new sub root
+	 */
+	private AVLTreeNode rightRotateLeftChild(AVLTreeNode n){
+		if(n == null || n.mLeft == null)
+			return n;
+		
+		AVLTreeNode newSubRoot = n.mLeft;
+		n.mLeft = newSubRoot.mRight;
+		newSubRoot.mRight = n;
+		
+		//Adjusting the heights of the old and new sub root.
+		newSubRoot.mHeight = Math.max(height(newSubRoot.mLeft),
+				height(newSubRoot.mRight)) + 1;
+		n.mHeight = Math.max(height(n.mLeft), height(n.mRight)) +1;
+		
+		return newSubRoot;
+	}
+	
+	/**
+	 * This method performs a left rotation on the right
+	 * child of the provided node.
+	 * @param n
+	 * @return the new sub root
+	 */
+	private AVLTreeNode leftRotateRightChild(AVLTreeNode n){
+		if(n == null || n.mRight == null)
+			return n;
+		
+		AVLTreeNode newSubRoot = n.mRight;
+		n.mRight = newSubRoot.mLeft;
+		newSubRoot.mLeft = n;
+		
+		//Adjusting the heights of the old and new sub root.
+		newSubRoot.mHeight = Math.max(height(newSubRoot.mLeft), 
+				height(newSubRoot.mRight)) + 1;
+		n.mHeight = Math.max(height(n.mLeft), height(n.mRight)) +1;
+		return newSubRoot;
+	}
+	
+	/**
+	 * Method to retrieve height of an avl node.
+	 * @param n- AVL node
+	 * @return height of the AVL node if provdided
+	 * otherwise returns -1.
+	 */
+	private int height(AVLTreeNode n){
+		return (n == null) ? -1 : n.mHeight;
+	}
+	
+	/**
+	 * Helper method to determine whether a rotation
+	 * is required or not.
+	 * @param n
+	 * @return difference of the height of the left
+	 * and right child of the provided node.
+	 */
+	private int difference(AVLTreeNode n){
+		return (n == null) ? 0 : height(n.mLeft) - height(n.mRight);
+	}
+}
---
+++ src/AVL/AVLTreeTest.java
@@ -0,0 +1,171 @@
+package AVL;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * This is a test class for AVLTree
+ * @author Arjun Passi
+ *
+ */
+public class AVLTreeTest {
+
+	/** Reference to the AVL tree on which testing will be performed*/
+	private AVLTree<Integer> mTestTree;
+	
+	@Before
+	public void setUp(){
+		mTestTree = new AVLTree<Integer>();
+	}
+	
+	/**
+	 * Test method for size()
+	 */
+	@Test
+	public void testSize(){
+		assertEquals(mTestTree.size(), 0);
+		mTestTree.insert(100);
+		assertEquals(mTestTree.size(), 1);
+		mTestTree.insert(100);
+		assertEquals(mTestTree.size(), 1);
+		mTestTree.insert(150);
+		assertEquals(mTestTree.size(), 2);
+		mTestTree.insert(200);
+		assertEquals(mTestTree.size(), 3);
+		mTestTree.insert(125);
+		assertEquals(mTestTree.size(), 4);
+	}
+	
+	/**
+	 * Test method for insert
+	 */
+	@Test
+	public void testInsert(){
+		assertEquals(mTestTree.size(), 0);
+		assertTrue(mTestTree.insert(100));
+		assertEquals(mTestTree.size(), 1);
+		assertEquals(mTestTree.find(100), (Integer) 100);
+		
+		assertFalse(mTestTree.insert(100));
+		assertEquals(mTestTree.size(), 1);
+		assertEquals(mTestTree.find(100), (Integer) 100);
+		
+		assertTrue(mTestTree.insert(150));
+		assertEquals(mTestTree.size(), 2);
+		assertEquals(mTestTree.find(150), (Integer) 150);
+		
+		assertTrue(mTestTree.insert(200));
+		assertEquals(mTestTree.size(), 3);
+		assertEquals(mTestTree.find(200), (Integer) 200);
+		
+		mTestTree.insert(125);
+		assertEquals(mTestTree.size(), 4);
+		assertEquals(mTestTree.find(125), (Integer) 125);
+		
+		
+		for(int i = 0; i < 50; i++){
+			assertTrue(mTestTree.insert(i));
+			assertEquals(mTestTree.size(), 5 + i);
+			assertEquals(mTestTree.find(i), (Integer) i);
+		}
+	}
+	
+	/**
+	 * Test method for find
+	 */
+	@Test
+	public void testFind(){
+		for(int i = 0; i < 100; i++){
+			assertEquals(mTestTree.find(i), null);
+			assertTrue(mTestTree.insert(i));
+			assertEquals(mTestTree.find(i), (Integer) i);
+		}
+	}
+	
+	/**
+	 * Test method for remove
+	 */
+	@Test
+	public void testRemove(){
+		assertFalse(mTestTree.remove(100));
+		assertFalse(mTestTree.remove(200));
+		assertFalse(mTestTree.remove(150));
+		
+		assertTrue(mTestTree.insert(150));
+		assertTrue(mTestTree.insert(100));
+		assertTrue(mTestTree.insert(200));
+		
+		assertTrue(mTestTree.remove(150));
+		assertEquals(mTestTree.find(150), null);
+		assertEquals(mTestTree.size(), 2);
+		assertEquals(mTestTree.find(100), (Integer) 100);
+		assertEquals(mTestTree.find(200), (Integer) 200);
+		
+		assertTrue(mTestTree.remove(100));
+		assertEquals(mTestTree.find(100), null);
+		assertEquals(mTestTree.size(), 1);
+		assertEquals(mTestTree.find(200), (Integer) 200);
+		
+		assertTrue(mTestTree.remove(200));
+		assertEquals(mTestTree.find(200), null);
+		assertEquals(mTestTree.size(), 0);
+		
+		for(int i = 0; i < 100; i++)
+			assertTrue(mTestTree.insert(i));
+		
+		for(int j = 0; j < 100; j++){
+			assertEquals(mTestTree.size(), 100 - j);
+			assertTrue(mTestTree.remove(j));
+			assertEquals(mTestTree.find(j), null);
+		}
+	}
+	
+	/**
+	 * Test method for removeAll
+	 */
+	@Test
+	public void testRemoveAll(){
+		for(int i = 0; i < 100; i++)
+			assertTrue(mTestTree.insert(i));
+		
+		mTestTree.removeAll();
+		
+		assertEquals(mTestTree.size(), 0);
+		
+		for(int j = 0; j < 100; j++)
+			assertEquals(mTestTree.find(j), null);
+	}
+	/**
+	 * Test method for inOrderTraversal
+	 */
+	@Test
+	public void testInOrderTraversal(){
+		assertTrue(mTestTree.insert(1));
+		assertTrue(mTestTree.insert(15));
+		assertTrue(mTestTree.insert(10));
+		assertTrue(mTestTree.insert(9));
+		assertTrue(mTestTree.insert(4));
+		assertTrue(mTestTree.insert(8));
+		assertTrue(mTestTree.insert(14));
+		assertTrue(mTestTree.insert(6));
+		assertTrue(mTestTree.insert(3));
+		assertTrue(mTestTree.insert(12));
+		assertTrue(mTestTree.insert(2));
+		assertTrue(mTestTree.insert(13));
+		assertTrue(mTestTree.insert(5));
+		assertTrue(mTestTree.insert(7));
+		assertTrue(mTestTree.insert(11));
+		
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		
+		for(int i = 1; i <= 15; i++)
+			list.add(i);
+		
+		assertEquals(list, mTestTree.inOrderTraversal());
+		
+	}
+
+}
