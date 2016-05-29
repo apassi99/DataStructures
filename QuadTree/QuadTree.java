2cec5b0ac6b5a20f1c0caacf8cb15d983884d055
apassi99 <apassi@live.com>
2016-05-29 11:26:55 -0400

    added remove and find in range

--- QuadTree/QuadTree.java
+++ QuadTree/QuadTree.java
@@ -1,8 +1,14 @@
 package QuadTree;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * A implementation of QuadTree that partitions a two dimensional
- * domain 
+ * domain by dividing it into four quadrants. The keys associated
+ * with the quad tree are stored in X-Y coordinate pairs.
+ * 
+ * This is not a thread safe implementation.
  * 
  * @author Arjun Passi
  *
@@ -10,6 +16,7 @@ package QuadTree;
 public class QuadTree <T>{
 
 	
+	/** Represents a quadrant direction. */
 	private enum Direction {NW, SW, SE, NE, NOQUADRANT};
 	
 	/**
@@ -95,8 +102,10 @@ public class QuadTree <T>{
 
 		@Override
 		public boolean intersects(RectangularDomain rectDomain) {
-			// TODO Auto-generated method stub
-			return false;
+			assert rectDomain != null;
+			
+			return m_xMin < rectDomain.m_xMax && m_xMax > rectDomain.m_xMin
+			          && m_yMin < rectDomain.m_yMax && m_yMax > rectDomain.m_yMin;
 		}
 
 		@Override
@@ -162,16 +171,36 @@ public class QuadTree <T>{
 	 */
 	private class QuadLeafNode extends QuadNode {
 		
+		/** Reference to a data entry in the leaf node. */
 		QuadTreeEntry m_entry;
 		
+		/**
+		 * Constructs a QuadLeafNode.
+		 * 
+		 * @param elem - data element
+		 * @param x - x-coordinate
+		 * @param y - y-coordinate
+		 */
 		QuadLeafNode(T elem, long x, long y) {
 			m_entry = new QuadTreeEntry(elem, x, y);
 		}
 		
+		/**
+		 * Constructs a QuadLeafNode.
+		 * 
+		 * @param entry - QuadTreeEntry
+		 */
 		QuadLeafNode(QuadTreeEntry entry) {
 			m_entry = entry;
 		}
 
+		/**
+		 * Method returns string representation
+		 * of the leaf node.
+		 * 
+		 * @return string representation of the leaf node.
+		 */
+		@Override
 		public String toString() {
 			StringBuilder build = new StringBuilder();
 			build.append(m_entry.toString());
@@ -187,6 +216,7 @@ public class QuadTree <T>{
 	 */
 	private class QuadInternalNode extends QuadNode {
 		
+		/** Reference to the four child nodes. */
 		QuadNode NW, NE, SE, SW;
 	      
 		/**
@@ -218,6 +248,10 @@ public class QuadTree <T>{
 	private static final long YMAX_DEFAULT = 100;
 	private static final long YMIN_DEFAULT = 0;
 	
+	/**
+	 * Constructs a QuadTree object with
+	 * default domain size.
+	 */
 	QuadTree() {
 		m_Root = null;
 		m_QuadTreeDomain = new RectangularDomain(
@@ -225,6 +259,41 @@ public class QuadTree <T>{
 						YMIN_DEFAULT, YMAX_DEFAULT);
 	}
 	
+	/**
+	 * Constructs a QuadTree object with the provided
+	 * domain size.
+	 * 
+	 * @param xlo - lower x-coordinate
+	 * @param xhi - upper x-coordinate
+	 * @param ylo - lower y-coordinate
+	 * @param yhi - upper y-coordinate
+	 */
+	QuadTree(long xlo, long xhi, long ylo, long yhi) {
+		
+		if (xlo > xhi) {
+			throw new IllegalArgumentException(
+					"xlo must be less than or equal to xhi.");
+		}
+		
+		if (ylo > yhi) {
+			throw new IllegalArgumentException(
+					"ylo must be less than or equal to yhi.");
+		}
+		    
+		m_Root = null;
+		m_QuadTreeDomain = new RectangularDomain(
+				xlo, xhi, ylo, yhi);
+	}
+	
+	/**
+	 * Method to insert a data item in the QuadTree.
+	 * 
+	 * @param elem - data element
+	 * @param x - x-coordinate
+	 * @param y - y-coordinate
+	 * @return True if insertion was successful otherwise False.
+	 * 		   Insertion fails if there exists a mapping at x-y.
+	 */
 	public boolean insert(T elem, long x, long y) {
 		
 		if(elem == null || !m_QuadTreeDomain.contains(x, y))
@@ -236,6 +305,14 @@ public class QuadTree <T>{
 		return entry.m_placed;
 	}
 	
+	/**
+	 * Helper recursive method to insert an item in the QuadTree.
+	 * 
+	 * @param n - Root node of the tree to insert the element in. 
+	 * @param entry - Data Entry to be inserted.
+	 * @param domain - domain of the subtree.
+	 * @return The new root of the specified sub tree.
+	 */
 	private QuadNode insert(QuadNode n, QuadTreeEntry entry, RectangularDomain domain) {
 		
 		if (n == null) {
@@ -285,6 +362,15 @@ public class QuadTree <T>{
 		return n;
 	}
 	
+	/**
+	 * Returns the value that is mapped by the provided
+	 * x-y coordinate pair.
+	 * 
+	 * @param x - x-coordinate
+	 * @param y - y-coordinate
+	 * @return Reference to the value or null (if no mapping or
+	 * 		   mapped value is null).
+	 */
 	public T find(long x, long y) {
 		
 		if ( !m_QuadTreeDomain.contains(x, y) ) {
@@ -294,6 +380,16 @@ public class QuadTree <T>{
 		return find(m_Root, x, y, m_QuadTreeDomain);
 	}
 	
+	/**
+	 * Helper recursive method to search within the quadtree given
+	 * x-y coordinate pairs.
+	 * 
+	 * @param n - Root node of the subtree to search in
+	 * @param x - x-coordinate
+	 * @param y - y-coordinate
+	 * @param domain - domain of the subtree.
+	 * @return Reference to the value or null (if no mapping).
+	 */
 	private T find(QuadNode n, long x, long y, RectangularDomain domain) {
 		
 		if (n == null) {
@@ -328,6 +424,177 @@ public class QuadTree <T>{
 		}
 		
 		return null;
+	}
+	
+	/**
+	 * Method to remove the element mapping to provided
+	 * x-y coordinate.
+	 * 
+	 * @param x - x-coordinate
+	 * @param y - y-coordinate
+	 * @return reference to the element removed or null if there was no mapping
+	 */
+	public T remove(long x, long y) {
+		if ( !m_QuadTreeDomain.contains(x, y) ) {
+			return null;
+		}
+		
+		QuadTreeEntry entry = new QuadTreeEntry(null, x, y);
+		m_Root = remove(m_Root, entry, x, y, m_QuadTreeDomain);
+		
+		return entry.m_element;
+	}
+	
+	/**
+	 * Helper method to recursively remove from an entry from
+	 * a QuadTree.
+	 * 
+	 * @param n - Root node of the subtree where entry is to be deleted
+	 * @param entry - data item to be removed
+	 * @param x - x-coordinate
+	 * @param y - y-coordinate
+	 * @param domain - current search domain
+	 * @return new root of the subtree with the node removed
+	 */
+	private QuadNode remove(QuadNode n, QuadTreeEntry entry, long x, long y, RectangularDomain domain) {
+		
+		if (n == null) {
+			return null;
+		}
+		
+		if (n instanceof QuadTree.QuadLeafNode) {
+			
+			QuadLeafNode temp = (QuadLeafNode) n;
+			
+			if (temp.m_entry.m_x == x && temp.m_entry.m_y == y) {
+				entry.m_element = temp.m_entry.m_element;
+				return null;
+			}
+		}
+		
+		else if (n instanceof QuadTree.QuadInternalNode) {
+			switch (domain.inQuadrant(x, y)) 
+			{
+				case NE:
+					((QuadInternalNode ) n).NE = remove(((QuadInternalNode ) n).NE, entry, 
+							x, y, modifyRectDomain(domain, Direction.NE));
+					break;
+				case NW:
+					((QuadInternalNode ) n).NW = remove(((QuadInternalNode ) n).NW, entry, 
+							x, y, modifyRectDomain(domain, Direction.NW));
+					break;
+				case SE:
+					((QuadInternalNode ) n).SE = remove(((QuadInternalNode ) n).SE, entry, 
+							x, y, modifyRectDomain(domain, Direction.SE));
+					break;
+				case SW:
+					((QuadInternalNode ) n).SW = remove(((QuadInternalNode ) n).SW, entry, 
+							x, y, modifyRectDomain(domain, Direction.SW));
+					break;
+			}
+			
+			n = checkAndUpdate((QuadInternalNode) n);
+		}
+		
+		return n;
+	}
+	
+	private QuadNode checkAndUpdate(QuadInternalNode n) {
+		if (n == null) {
+			return null;
+		}
+		
+		int count = 0;
+		
+		if (n.NE == null) count++;
+		if (n.NW == null) count++;
+		if (n.SE == null) count++;
+		if (n.SW == null) count++;
+		
+		if (count != 3)
+			return n;
+		
+		if(n.NE != null && n.NE instanceof QuadTree.QuadLeafNode)
+			return n.NE;
+		else if(n.NW != null && n.NW instanceof QuadTree.QuadLeafNode)
+			return n.NW;
+		else if(n.SW != null && n.SW instanceof QuadTree.QuadLeafNode)
+			return n.SW;
+		else if (n.SE != null && n.SE instanceof QuadTree.QuadLeafNode)
+			return n.SE;
+		else
+			return n;
+	}
+	
+	/**
+	 * Method to return list of items in the provided
+	 * rectangular domain.
+	 * 
+	 * @param xlo
+	 * @param xhi
+	 * @param ylo
+	 * @param yhi
+	 * @return list of items in the provided rectangular domain
+	 */
+	public List<T> find(long xlo, long xhi, long ylo, long yhi) {
+		
+		List<T> list = new LinkedList<T>();
+		RectangularDomain searchDomain = new RectangularDomain(xlo, xhi, ylo, yhi);
+		
+		if (m_QuadTreeDomain.intersects(searchDomain)) {
+			find(m_Root, m_QuadTreeDomain, searchDomain, list);
+		}
+		
+		return list;
+	}
+	
+	/**
+	 * Helper method to recursively search for elements in a provided
+	 * search domain.
+	 * 
+	 * @param n - Sub root node of the tree to search in 
+	 * @param treeDomain - current tree domain
+	 * @param searchDomain - search domain
+	 * @param list - list of elements in the search domain
+	 */
+	private void find(QuadNode n, RectangularDomain treeDomain, Domain searchDomain, List<T> list) {
+		
+		if (n == null) {
+			return;
+		}
+		
+		if (n instanceof QuadTree.QuadLeafNode) {
+			QuadLeafNode temp = (QuadLeafNode) n;
+			list.add(temp.m_entry.m_element);
+		}
+		
+		else if (n instanceof QuadTree.QuadInternalNode) {
+			
+			QuadInternalNode temp = (QuadInternalNode) n;
+			RectangularDomain newDomain = modifyRectDomain(treeDomain, Direction.NE);
+			
+			if (searchDomain.intersects(newDomain)) {
+				find(temp.NE, newDomain, searchDomain, list);
+			}
+			
+			newDomain = modifyRectDomain(treeDomain, Direction.NW);
+			
+			if (searchDomain.intersects(newDomain)) {
+				find(temp.NW, newDomain, searchDomain, list);
+			}
+			
+			newDomain = modifyRectDomain(treeDomain, Direction.SW);
+			
+			if (searchDomain.intersects(newDomain)) {
+				find(temp.SW, newDomain, searchDomain, list);
+			} 
+			
+			newDomain = modifyRectDomain(treeDomain, Direction.SE);
+			
+			if (searchDomain.intersects(newDomain)) {
+				find(temp.SE, newDomain, searchDomain, list);
+			} 
+		}
 	}
 	
 	/**
